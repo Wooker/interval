9a004bc5e81a92d01c3779980384c43609b56283
wooker <zakhar.semyonov@gmail.com>
2023-12-25 12:42:11 +0600

    Change overlap ordering names

--- src/interval_building/interval.rs
+++ src/interval_building/interval.rs
@@ -4,16 +4,16 @@ use comparable_bound::ComparableBound;
 
 #[derive(Debug, PartialEq, Eq)]
 pub enum OverlapOrdering {
-    SuperSet,            // (1, 4) in relation to (2, 3)
-    SubSet,              // (2, 3) in relation to (1, 4)
-    Less,                // (1, 2) in relation to (3, 4)
-    OverlapLess,         // (1, 3) in relation to (2, 4)
-    OverlapEqualLess,    // (1, 2) in relation to (2, 4)
-    Greater,             // (3, 4) in relation to (1, 2)
-    OverlapGreater,      // (2, 4) in relation to (1, 3)
-    OverlapEqualGreater, // (3, 4) in relation to (1, 3)
-    Equal,               // (1, 2) (1, 2)
-    InvalidInterval,
+    SuperSet,       // (1, 4) in relation to (2, 3)
+    SubSet,         // (2, 3) in relation to (1, 4)
+    Less,           // (1, 2) in relation to (3, 4)
+    OverlapStart,   // (1, 3) in relation to (2, 4)
+    EqualStart,     // (1, 2] in relation to [2, 4)
+    Greater,        // (3, 4) in relation to (1, 2)
+    OverlapEnd,     // (2, 4) in relation to (1, 3)
+    EqualEnd,       // (3, 4) in relation to (1, 3)
+    Equal,          // (1, 2) (1, 2)
+    Unrecognizable, //
 }
 
 #[derive(Debug, PartialEq, Eq, PartialOrd, Clone)]
@@ -51,46 +51,50 @@ where
             self.end.partial_cmp(&other.start),
         ) {
             (Some(_), Some(_)) => self.cross_compare(other),
-            _ => OverlapOrdering::InvalidInterval,
+            _ => OverlapOrdering::Unrecognizable,
         }
     }
 
+    /// Compares _start_ of self interval with _end_ of other interval and
+    /// _end_ of self interval with _start_ of other interval and
     fn cross_compare(&self, other: &Self) -> OverlapOrdering {
         match (
             self.start.partial_cmp(&other.end).unwrap(),
             self.end.partial_cmp(&other.start).unwrap(),
         ) {
             (Ordering::Less, Ordering::Less) => OverlapOrdering::Less,
-            (Ordering::Less, Ordering::Equal) => OverlapOrdering::OverlapEqualLess,
+            (Ordering::Less, Ordering::Equal) => OverlapOrdering::EqualStart,
             (Ordering::Less, Ordering::Greater) => {
                 dbg!("Checking for subset");
                 self.direct_compare(other) // overlaps for sure
             }
             (Ordering::Equal, Ordering::Equal) => OverlapOrdering::Equal,
-            (Ordering::Equal, Ordering::Greater) => OverlapOrdering::OverlapEqualGreater,
+            (Ordering::Equal, Ordering::Greater) => OverlapOrdering::EqualEnd,
             (Ordering::Greater, Ordering::Greater) => OverlapOrdering::Greater,
 
             // (Ordering::Equal, Ordering::Less)
             // (Ordering::Greater, Ordering::Less)
             // (Ordering::Greater, Ordering::Equal)
-            _ => OverlapOrdering::InvalidInterval,
+            _ => OverlapOrdering::Unrecognizable,
         }
     }
 
+    /// Compares _start_ of self interval with _start_ of other interval and
+    /// _end_ of self interval with _end_ of other interval and
     fn direct_compare(&self, other: &Self) -> OverlapOrdering {
         match (
             self.start.partial_cmp(&other.start).unwrap(),
             self.end.partial_cmp(&other.end).unwrap(),
         ) {
-            (Ordering::Less, Ordering::Less) => OverlapOrdering::OverlapGreater,
-            (Ordering::Less, Ordering::Equal) => OverlapOrdering::OverlapEqualGreater,
+            (Ordering::Less, Ordering::Less) => OverlapOrdering::OverlapStart,
+            (Ordering::Less, Ordering::Equal) => OverlapOrdering::EqualEnd,
             (Ordering::Less, Ordering::Greater) => OverlapOrdering::SuperSet,
-            (Ordering::Equal, Ordering::Greater) => OverlapOrdering::OverlapEqualLess,
+            (Ordering::Equal, Ordering::Greater) => OverlapOrdering::EqualStart,
             (Ordering::Equal, Ordering::Less) => OverlapOrdering::SubSet,
             (Ordering::Equal, Ordering::Equal) => OverlapOrdering::Equal,
             (Ordering::Greater, Ordering::Less) => OverlapOrdering::SubSet,
             (Ordering::Greater, Ordering::Equal) => OverlapOrdering::SubSet,
-            (Ordering::Greater, Ordering::Greater) => OverlapOrdering::OverlapLess,
+            (Ordering::Greater, Ordering::Greater) => OverlapOrdering::OverlapEnd,
         }
     }
 
